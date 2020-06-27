c52a1417958d2d717c19fd43377fc1c1b06720c3
Unknown <francescofavaro3@gmail.com>
2020-06-27 12:06:05 +0200

    getRegionCount

--- api/routes/mapmarkers.js
+++ api/routes/mapmarkers.js
@@ -108,7 +108,7 @@ router.patch("/addRegion", checkAuth, (req, res) => {
 
 router.get("/getRegionCount", (req, res) => {
   let sql =
-    "SELECT regione, COUNT(regione) AS quantita FROM maps_markers WHERE regione IS NOT NULL GROUP BY regione";
+    "SELECT regione, COUNT(regione) AS quantita FROM maps_markers WHERE regione IS NOT NULL GROUP BY regione ORDER BY quantita";
 
   db.query(sql, (err, result) => {
     if (err) {
